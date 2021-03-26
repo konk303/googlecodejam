92d3e042ad8f7bdb5f33db8ca6d724e0344c35d9
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2021-03-27 00:47:07 +0900

    cjc

---
+++ 2021/qualification/moons_and_umbrellas/solve.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'pry'
+
+class Solver
+  attr_reader :cj, :jc, :row
+
+  def initialize(cj, jc, row)
+    @cj = cj
+    @jc = jc
+    @row = row.delete('?')
+  end
+
+  def result
+    cj * row.scan('CJ').size + jc * row.scan('JC').size
+  end
+end
+
+T = gets.to_i
+T.times do |t|
+  cj, jc, row = gets.chomp.split(' ')
+  puts "Case ##{t.succ}: #{Solver.new(cj.to_i, jc.to_i, row).result}"
+end
